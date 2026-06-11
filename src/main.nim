a5588e64a04850d470fe00a840bb7648bec84c01
Jakob Friedl <71284620+jakobfriedl@users.noreply.github.com>
2026-06-11 14:39:11 +0200

    BOF entry point can be passed as an argument to the async BOF.

--- src/main.nim
+++ src/main.nim
@@ -7,7 +7,7 @@ proc DllMain(hinstDLL: HINSTANCE, fdwReason: DWORD, lpvReserved: LPVOID): BOOL
              {.stdcall, exportc, dynlib.} =
     return TRUE
 
-proc parseArguments(args: PBYTE, argsLen: DWORD): tuple[obj: PBYTE, objLen: DWORD, objArgs: PBYTE, objArgsLen: DWORD] =
+proc parseArguments(args: PBYTE, argsLen: DWORD): tuple[obj: PBYTE, objLen: DWORD, objArgs: PBYTE, objArgsLen: DWORD, entryFunc: string] =
     if args == nil or argsLen < 8:
         return
 
@@ -19,10 +19,16 @@ proc parseArguments(args: PBYTE, argsLen: DWORD): tuple[obj: PBYTE, objLen: DWOR
     if argsLen < cast[DWORD](4 + bofLen + 4 + bofArgsLen):
         return
 
+    let entryFuncLen = cast[ptr uint32](cast[uint](args) + 4 + bofLen + 4 + bofArgsLen)[]
+    if argsLen < cast[DWORD](4 + bofLen + 4 + bofArgsLen + 4 + entryFuncLen):
+        return
+
     result.obj = cast[PBYTE](cast[uint](args) + 4)
     result.objLen = cast[DWORD](bofLen)
     result.objArgs = cast[PBYTE](cast[uint](args) + 4 + bofLen + 4)
-    result.objArgsLen = cast[DWORD](bofArgsLen)
+    result.objArgsLen = cast[DWORD](bofArgsLen)    
+    result.entryFunc = newString(entryFuncLen)
+    copyMem(addr result.entryFunc[0], cast[cstring](cast[uint](args) + 4 + bofLen + 4 + bofArgsLen + 4), entryFuncLen)
 
 proc Run(args: PBYTE, argsLen: DWORD, hWrite, hWakeup, hStop: HANDLE): BOOL {.stdcall, exportc, dynlib.} =
     NimMain()
@@ -31,9 +37,9 @@ proc Run(args: PBYTE, argsLen: DWORD, hWrite, hWakeup, hStop: HANDLE): BOOL {.st
     gWakeupEvent = hWakeup
     gStopEvent = hStop
 
-    let (obj, objLen, objArgs, objArgsLen) = parseArguments(args, argsLen)
+    let (obj, objLen, objArgs, objArgsLen, entryFunc) = parseArguments(args, argsLen)
     try:
-        inlineExecute(obj, objLen, objArgs, objArgsLen)
+        inlineExecute(obj, objLen, objArgs, objArgsLen, entryFunc)
     
     except CatchableError as err:
         # Write error message to pipe and wakeup agent
